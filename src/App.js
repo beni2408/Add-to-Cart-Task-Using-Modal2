ae391bd7ddf3ec1001943820778aebcb54243074
beni2408 <jascarbenish@gmail.com>
2025-09-20 15:29:16 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
 import CartModal from "./components/CartModal";
